04a404679300a5115e66044eda41b23dfa5fa144
Huyen Trang Dinh <tran9dinh@gmail.com>
2021-01-15 16:51:00 -0500

    instance creation

--- main.cpp
+++ main.cpp
@@ -19,6 +19,8 @@ public:
 	}
 private:
 	GLFWwindow* window;
+	VkInstance instance;
+
 
 	void initWindow()
 	{
@@ -30,7 +32,7 @@ private:
 
 	void initVulkan()
 	{
-
+		createInstance();
 	}
 
 	void mainLoop()
@@ -45,6 +47,32 @@ private:
 	{
 		glfwDestroyWindow(window);
 		glfwTerminate();
+	}
+
+	void createInstance()
+	{
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
 	}
 };
 
